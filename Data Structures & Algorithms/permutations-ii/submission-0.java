b7b5d4ad58cfb6c3cd65cab7ecc95014defc334a
Harshita Chabaria <136854637+harshitachabaria24@users.noreply.github.com>
2026-07-09 17:35:28 +0530

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        boolean[] visited=new boolean[nums.length];
+        helper(nums,new ArrayList<>(),visited,ans);
+        return ans;
+    }
+    public void helper(int nums[],List<Integer> path,boolean[] visited,List<List<Integer>> ans){
+        if(path.size()==nums.length){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(visited[i]){
+                continue;
+            }
+            if(i>0 && nums[i]==nums[i-1] && !visited[i-1]){
+                continue;
+            }
+            path.add(nums[i]);
+            visited[i]=true;
+            helper(nums,path,visited,ans);
+            path.remove(path.size()-1);
+            visited[i]=false;
+
+        }
+    }
+}
