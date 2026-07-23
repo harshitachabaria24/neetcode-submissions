2523a2289abbd6112199fa30c108cb4e3adae895
Harshita Chabaria <136854637+harshitachabaria24@users.noreply.github.com>
2026-07-23 12:39:22 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    int[] dp;
+    public int climbStairs(int n) {
+        
+        dp=new int[n+1];
+        return climb(n);
+    }
+    private int climb(int n){
+        if(n<=2){
+            return n;
+        }
+        if(dp[n]!=0)
+        return dp[n];
+
+        dp[n]=climb(n-1)+climb(n-2);
+        return dp[n];
+    }
+}
