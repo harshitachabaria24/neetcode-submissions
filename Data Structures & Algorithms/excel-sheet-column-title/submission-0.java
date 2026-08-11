bba38e2313bc3827d59251a251da5c7d3e563069
Harshita Chabaria <136854637+harshitachabaria24@users.noreply.github.com>
2026-08-11 12:51:47 +0530

    Add: excel-sheet-column-title - submission-0

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb=new StringBuilder();
+        while(columnNumber>0){
+            columnNumber--;
+            int remainder=columnNumber % 26;
+            char ch=(char)('A'+remainder);
+            sb.append(ch);
+            columnNumber /= 26;
+
+        }
+        return sb.reverse().toString();
+    }
+}
